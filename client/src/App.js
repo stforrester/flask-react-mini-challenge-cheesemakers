397819c5d21d1a780006b7d9752055eeea6876fc
betalantz <betalantz@gmail.com>
2023-08-20 22:59:18 -0700

    merge chakra setup files

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,67 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-
+import React from 'react';
+import {
+  ChakraProvider,
+  Box,
+  Heading,
+  Link,
+  VStack,
+  Grid,
+  theme,
+} from '@chakra-ui/react';
+import { Link as RouteLink, Routes, Route } from 'react-router-dom';
+import { ColorModeSwitcher } from './ColorModeSwitcher';
+import Home from './components/Home';
+import ProducerDetail from './components/ProducerDetail';
+import CheeseForm from './components/CheeseForm';
+import CheeseEdit from './components/CheeseEdit';
 
 function App() {
-
-  const [producers, setProducers] = useState([])
-
-  useEffect(() => {
-    fetch('/producers')
-      .then(res => res.json())
-      .then(data => setProducers(data))
-  }, [])
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>CheeseBook</h1>
-        <ul>
-          {producers.map(producer => <li key={producer.id}>{producer.name}</li>)}
-        </ul>
-      </header>
-    </div>
+    <ChakraProvider theme={theme}>
+      <Box
+        textAlign="center"
+        fontSize="xl"
+      >
+        <Grid
+          minH="100vh"
+          p={3}
+        >
+          <ColorModeSwitcher justifySelf="flex-end" />
+          <VStack spacing={8}>
+            <Link
+              as={RouteLink}
+              to="/"
+            >
+              <Heading
+                as="h1"
+                size="3xl"
+              >
+                CheeseBook
+              </Heading>
+            </Link>
+
+            <Routes>
+              <Route
+                path="/"
+                element={<Home />}
+              />
+              <Route
+                path="/producers/:id/cheeses/new"
+                element={<CheeseForm />}
+              />
+              <Route
+                path="/producers/:id/cheeses/:cheeseId/edit"
+                element={<CheeseEdit />}
+              />
+              <Route
+                path="/producers/:id"
+                element={<ProducerDetail />}
+              />
+            </Routes>
+          </VStack>
+        </Grid>
+      </Box>
+    </ChakraProvider>
   );
 }
 
